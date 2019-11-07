c1daac24d4cfb109d8741b04318569c05e19a05b
Steffen Kroggel <kroggel@rkw.de>
2019-11-07 12:26:10 +0100

    [REFACTORING] user_func for ext_localconf.php and ext_tables.php / fix for flashMessages

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,215 +1,226 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(
+    function($extKey)
+    {
+
+        //=================================================================
+        // Configure Plugins
+        //=================================================================
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwconsultant',
+            array(
+                'Consultant' => 'list, show, preview, error, disable, enable',
+                'Ajax' => 'filter',
+            ),
+            // non-cacheable actions
+            array(
+                'Consultant' => 'list, show, preview, error, disable, enable',
+                'Ajax' => 'filter',
+            )
+        );
 
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwconsultant',
-	array(
-		'Consultant' => 'list, show, preview, error, disable, enable',
-        'Ajax' => 'filter',
-    ),
-	// non-cacheable actions
-	array(
-		'Consultant' => 'list, show, preview, error, disable, enable',
-        'Ajax' => 'filter',
-    )
-);
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwconsultantmyconsultant',
+            array(
+                'Consultant' => 'myList, show, new, create, edit, update, prepareDelete, delete, error, removeFile, maps',
+                'ConsultantService' => 'new, create, edit, update, delete, choose, removeFile',
+            ),
+            // non-cacheable actions
+            array(
+                'Consultant' => 'myList, show, new, create, edit, update, prepareDelete, delete, error, removeFile, maps',
+                'ConsultantService' => 'new, create, edit, update, delete, choose, removeFile',
+            )
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwconsultantmaps',
+            array(
+                'Consultant' => 'maps',
+            ),
+            // non-cacheable actions
+            array(
+                'Consultant' => 'maps',
+            )
+        );
 
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwconsultantmyconsultant',
-	array(
-		'Consultant' => 'myList, show, new, create, edit, update, prepareDelete, delete, error, removeFile, maps',
-		'ConsultantService' => 'new, create, edit, update, delete, choose, removeFile',
-	),
-	// non-cacheable actions
-	array(
-		'Consultant' => 'myList, show, new, create, edit, update, prepareDelete, delete, error, removeFile, maps',
-		'ConsultantService' => 'new, create, edit, update, delete, choose, removeFile',
-	)
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwconsultantmaps',
-	array(
-		'Consultant' => 'maps',
-	),
-	// non-cacheable actions
-	array(
-		'Consultant' => 'maps',
-	)
-);
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwconsultantgallery',
+            array(
+                'Consultant' => 'gallery',
+            ),
+            // non-cacheable actions
+            array(
+                'Consultant' => 'gallery',
+            )
+        );
 
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwconsultantgallery',
-	array(
-		'Consultant' => 'gallery',
-	),
-	// non-cacheable actions
-	array(
-		'Consultant' => 'gallery',
-	)
-);
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwconsultantinfo',
+            array(
+                'Consultant' => 'info',
+            ),
+            // non-cacheable actions
+            array(
+                'Consultant' => 'info',
+            )
+        );
 
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwconsultantinfo',
-	array(
-		'Consultant' => 'info',
-	),
-	// non-cacheable actions
-	array(
-		'Consultant' => 'info',
-	)
-);
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'RKW.' . $extKey,
+            'Rkwconsultantcompany',
+            array(
+                'Consultant' => 'company',
+            ),
+            // non-cacheable actions
+            array(
+                'Consultant' => 'company',
+            )
+        );
 
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'RKW.' . $_EXTKEY,
-	'Rkwconsultantcompany',
-	array(
-		'Consultant' => 'company',
-	),
-	// non-cacheable actions
-	array(
-		'Consultant' => 'company',
-	)
-);
+        //=================================================================
+        // Configure Signal-Slots
+        //=================================================================
+        /**
+         * @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher
+         */
+        $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
+        $signalSlotDispatcher->connect(
+            'RKW\\RkwConsultant\\Controller\\ConsultantController',
+            \RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_AFTER_EDITING_CONSULTANT,
+            'RKW\\RkwConsultant\\Service\\RkwMailService',
+            'sendAdminApproveMail'
+        );
+        $signalSlotDispatcher->connect(
+            'RKW\\RkwConsultant\\Controller\\ConsultantServiceController',
+            \RKW\RkwConsultant\Controller\ConsultantServiceController::SIGNAL_AFTER_CREATING_CONSULTANTSERVICE,
+            'RKW\\RkwConsultant\\Service\\RkwMailService',
+            'sendAdminApproveMail'
+        );
+        $signalSlotDispatcher->connect(
+            'RKW\\RkwConsultant\\Controller\\ConsultantServiceController',
+            \RKW\RkwConsultant\Controller\ConsultantServiceController::SIGNAL_AFTER_UPDATING_CONSULTANTSERVICE,
+            'RKW\\RkwConsultant\\Service\\RkwMailService',
+            'sendAdminApproveMail'
+        );
+        $signalSlotDispatcher->connect(
+            'RKW\\RkwConsultant\\Controller\\ConsultantServiceController',
+            \RKW\RkwConsultant\Controller\ConsultantServiceController::SIGNAL_AFTER_DELETING_CONSULTANTSERVICE,
+            'RKW\\RkwConsultant\\Service\\RkwMailService',
+            'sendAdminApproveMail'
+        );
 
+        $signalSlotDispatcher->connect(
+            'RKW\\RkwConsultant\\Controller\\ConsultantController',
+            \RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_AFTER_DELETE_CONSULTANT_ADMIN,
+            'RKW\\RkwConsultant\\Service\\RkwMailService',
+            'sendAdminDeleteMail'
+        );
 
-/**
- * Hook
- */
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$_EXTKEY] = 'RKW\\RkwConsultant\\Hooks\\TceMainHooks';
+        $signalSlotDispatcher->connect(
+            'RKW\\RkwConsultant\\Controller\\ConsultantController',
+            \RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_AFTER_DELETE_CONSULTANT_USER,
+            'RKW\\RkwConsultant\\Service\\RkwMailService',
+            'sendUserDeleteMail'
+        );
 
-// set logger
-$GLOBALS['TYPO3_CONF_VARS']['LOG']['RKW']['RkwConsultant']['writerConfiguration'] = array(
+        $signalSlotDispatcher->connect(
+            'RKW\\RkwConsultant\\Controller\\ConsultantController',
+            \RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_AFTER_UNLOCKING_CONSULTANT_USER,
+            'RKW\\RkwConsultant\\Service\\RkwMailService',
+            'sendUserUnlockMail'
+        );
+        $signalSlotDispatcher->connect(
+            'RKW\\RkwRegistration\\Tools\\Registration',
+            \RKW\RkwRegistration\Tools\Registration::SIGNAL_AFTER_DELETING_USER,
+            'RKW\\RkwConsultant\\Controller\\ConsultantController',
+            'removeAllOfUserSignalSlot'
+        );
 
-	// configuration for WARNING severity, including all
-	// levels with higher severity (ERROR, CRITICAL, EMERGENCY)
-	\TYPO3\CMS\Core\Log\LogLevel::DEBUG => array(
-		// add a FileWriter
-		'TYPO3\\CMS\\Core\\Log\\Writer\\FileWriter' => array(
-			// configuration for the writer
-			'logFile' => 'typo3temp/logs/tx_rkwconsultant.log'
-		)
-	),
-);
+        /** @toDo: Finally delete when tested
+        if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('rkw_search')) {
 
-
-/**
- * @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher
- */
-$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
-
-$signalSlotDispatcher->connect(
-	'RKW\\RkwConsultant\\Controller\\ConsultantController',
-	\RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_AFTER_EDITING_CONSULTANT,
-	'RKW\\RkwConsultant\\Service\\RkwMailService',
-	'sendAdminApproveMail'
-);
-$signalSlotDispatcher->connect(
-	'RKW\\RkwConsultant\\Controller\\ConsultantServiceController',
-	\RKW\RkwConsultant\Controller\ConsultantServiceController::SIGNAL_AFTER_CREATING_CONSULTANTSERVICE,
-	'RKW\\RkwConsultant\\Service\\RkwMailService',
-	'sendAdminApproveMail'
-);
-$signalSlotDispatcher->connect(
-	'RKW\\RkwConsultant\\Controller\\ConsultantServiceController',
-	\RKW\RkwConsultant\Controller\ConsultantServiceController::SIGNAL_AFTER_UPDATING_CONSULTANTSERVICE,
-	'RKW\\RkwConsultant\\Service\\RkwMailService',
-	'sendAdminApproveMail'
-);
-$signalSlotDispatcher->connect(
-	'RKW\\RkwConsultant\\Controller\\ConsultantServiceController',
-	\RKW\RkwConsultant\Controller\ConsultantServiceController::SIGNAL_AFTER_DELETING_CONSULTANTSERVICE,
-	'RKW\\RkwConsultant\\Service\\RkwMailService',
-	'sendAdminApproveMail'
-);
-
-$signalSlotDispatcher->connect(
-	'RKW\\RkwConsultant\\Controller\\ConsultantController',
-	\RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_AFTER_DELETE_CONSULTANT_ADMIN,
-	'RKW\\RkwConsultant\\Service\\RkwMailService',
-	'sendAdminDeleteMail'
-);
-
-$signalSlotDispatcher->connect(
-	'RKW\\RkwConsultant\\Controller\\ConsultantController',
-	\RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_AFTER_DELETE_CONSULTANT_USER,
-	'RKW\\RkwConsultant\\Service\\RkwMailService',
-	'sendUserDeleteMail'
-);
-
-$signalSlotDispatcher->connect(
-	'RKW\\RkwConsultant\\Controller\\ConsultantController',
-	\RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_AFTER_UNLOCKING_CONSULTANT_USER,
-	'RKW\\RkwConsultant\\Service\\RkwMailService',
-	'sendUserUnlockMail'
-);
-
-$signalSlotDispatcher->connect(
-	'RKW\\RkwRegistration\\Tools\\Registration',
-	\RKW\RkwRegistration\Tools\Registration::SIGNAL_AFTER_DELETING_USER,
-	'RKW\\RkwConsultant\\Controller\\ConsultantController',
-	'removeAllOfUserSignalSlot'
-);
-
-/** @toDo: Finally delete when tested
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('rkw_search')) {
-
-	$signalSlotDispatcher->connect(
-		'RKW\\RkwConsultant\\Controller\\ConsultantController',
-		\RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_CONSULTANT_FOR_SEARCH,
-		'RKW\\RkwConsultant\\Service\\RkwSearchService',
-		'consultant'
-	);
-
-	$signalSlotDispatcher->connect(
-		'RKW\\RkwConsultant\\Controller\\ConsultantServiceController',
-		\RKW\RkwConsultant\Controller\ConsultantServiceController::SIGNAL_CONSULTANTSERVICE_FOR_SEARCH,
-		'RKW\\RkwConsultant\\Service\\RkwSearchService',
-		'consultantService'
-	);
-
-	// Signal Slot for varnish-extension
-	if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('varnish')) {
-
-		$signalSlotDispatcher->connect(
-			'RKW\\RkwConsultant\\Service\\RkwSearchService',
-			\RKW\RkwConsultant\Service\RkwSearchService::SIGNAL_CLEAR_PAGE_VARNISH,
-			'RKW\\RkwConsultant\\Service\\VarnishService',
-			'clearCacheOfPageEvent'
-		);
-	}
-}
-*/
-
-
-// Signal Slot for varnish-extension
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('varnish')) {
-
-    $signalSlotDispatcher->connect(
+        $signalSlotDispatcher->connect(
         'RKW\\RkwConsultant\\Controller\\ConsultantController',
-        \RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_CONSULTANT_FOR_VARNISH,
-        'RKW\\RkwConsultant\\Service\\VarnishService',
-        'clearCacheOfPageEvent'
-    );
+        \RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_CONSULTANT_FOR_SEARCH,
+        'RKW\\RkwConsultant\\Service\\RkwSearchService',
+        'consultant'
+        );
 
-    $signalSlotDispatcher->connect(
+        $signalSlotDispatcher->connect(
         'RKW\\RkwConsultant\\Controller\\ConsultantServiceController',
-        \RKW\RkwConsultant\Controller\ConsultantServiceController::SIGNAL_CONSULTANTSERVICE_FOR_VARNISH,
+        \RKW\RkwConsultant\Controller\ConsultantServiceController::SIGNAL_CONSULTANTSERVICE_FOR_SEARCH,
+        'RKW\\RkwConsultant\\Service\\RkwSearchService',
+        'consultantService'
+        );
+
+        // Signal Slot for varnish-extension
+        if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('varnish')) {
+
+        $signalSlotDispatcher->connect(
+        'RKW\\RkwConsultant\\Service\\RkwSearchService',
+        \RKW\RkwConsultant\Service\RkwSearchService::SIGNAL_CLEAR_PAGE_VARNISH,
         'RKW\\RkwConsultant\\Service\\VarnishService',
         'clearCacheOfPageEvent'
-    );
-}
+        );
+        }
+        }
+         */
+
+        // Signal Slot for varnish-extension
+        if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('varnish')) {
+
+            $signalSlotDispatcher->connect(
+                'RKW\\RkwConsultant\\Controller\\ConsultantController',
+                \RKW\RkwConsultant\Controller\ConsultantController::SIGNAL_CONSULTANT_FOR_VARNISH,
+                'RKW\\RkwConsultant\\Service\\VarnishService',
+                'clearCacheOfPageEvent'
+            );
+
+            $signalSlotDispatcher->connect(
+                'RKW\\RkwConsultant\\Controller\\ConsultantServiceController',
+                \RKW\RkwConsultant\Controller\ConsultantServiceController::SIGNAL_CONSULTANTSERVICE_FOR_VARNISH,
+                'RKW\\RkwConsultant\\Service\\VarnishService',
+                'clearCacheOfPageEvent'
+            );
+        }
+
+        //=================================================================
+        // Configure Hook
+        //=================================================================
+        $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$extKey] = 'RKW\\RkwConsultant\\Hooks\\TceMainHooks';
+
+
+        //=================================================================
+        // Configure Logger
+        //=================================================================
+        $GLOBALS['TYPO3_CONF_VARS']['LOG']['RKW'][$extKey]['writerConfiguration'] = array(
+
+            // configuration for WARNING severity, including all
+            // levels with higher severity (ERROR, CRITICAL, EMERGENCY)
+            \TYPO3\CMS\Core\Log\LogLevel::DEBUG => array(
+
+                // add a FileWriter
+                'TYPO3\\CMS\\Core\\Log\\Writer\\FileWriter' => array(
+                    // configuration for the writer
+                    'logFile' => 'typo3temp/logs/tx_rkwconsultant.log'
+                )
+            ),
+        );
+
+    },
+    $_EXTKEY
+);
 
 
--- ext_tables.php
+++ ext_tables.php
@@ -1,87 +1,114 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(
+    function($extKey)
+    {
+
+        //=================================================================
+        // Register Plugins
+        //=================================================================
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Rkwconsultant',
+            'RKW Consultant'
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Rkwconsultantmyconsultant',
+            'RKW Consultant - MyList'
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Rkwconsultantmaps',
+            'RKW Consultant - GoogleMaps'
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Rkwconsultantgallery',
+            'RKW Consultant - Gallery'
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Rkwconsultantinfo',
+            'RKW Consultant - Info'
+        );
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Rkwconsultantcompany',
+            'RKW Consultant - Company'
+        );
+
+        //=================================================================
+        // Add tables
+        //=================================================================
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwconsultant_domain_model_basicservice'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwconsultant_domain_model_consultant'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwconsultant_domain_model_consultantservice'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwconsultant_domain_model_contactperson'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwconsultant_domain_model_qualification'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+            'tx_rkwconsultant_domain_model_subservice'
+        );
+
+        //=================================================================
+        // Add TypoScript
+        //=================================================================
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $extKey,
+            'Configuration/TypoScript',
+            'RKW Consultant'
+        );
 
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Rkwconsultant',
-	'RKW Consultant'
+        //=================================================================
+        // Add Flexform
+        //=================================================================
+        $extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($extKey));
+        $pluginName = strtolower('Rkwconsultantmyconsultant');
+        $pluginSignature = $extensionName.'_'.$pluginName;
+
+        $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
+        $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+            $pluginSignature,
+            'FILE:EXT:'.$extKey . '/Configuration/FlexForms/MyConsultant.xml'
+        );
+
+
+        $pluginName = strtolower('Rkwconsultant');
+        $pluginSignature = $extensionName.'_'.$pluginName;
+
+        $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
+        $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+            $pluginSignature,
+            'FILE:EXT:'.$extKey . '/Configuration/FlexForms/Consultant.xml'
+        );
+
+
+    },
+    $_EXTKEY
 );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Rkwconsultantmyconsultant',
-	'RKW Consultant - MyList'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Rkwconsultantmaps',
-	'RKW Consultant - GoogleMaps'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Rkwconsultantgallery',
-	'RKW Consultant - Gallery'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Rkwconsultantinfo',
-	'RKW Consultant - Info'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Rkwconsultantcompany',
-	'RKW Consultant - Company'
-);
-
-//=================================================================
-// Add tables
-//=================================================================
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwconsultant_domain_model_basicservice', 'EXT:rkw_consultant/Resources/Private/Language/locallang_csh_tx_rkwconsultant_domain_model_basicservice.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwconsultant_domain_model_basicservice');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwconsultant_domain_model_consultant', 'EXT:rkw_consultant/Resources/Private/Language/locallang_csh_tx_rkwconsultant_domain_model_consultant.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwconsultant_domain_model_consultant');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwconsultant_domain_model_consultantservice', 'EXT:rkw_consultant/Resources/Private/Language/locallang_csh_tx_rkwconsultant_domain_model_consultantservice.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwconsultant_domain_model_consultantservice');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwconsultant_domain_model_contactperson', 'EXT:rkw_consultant/Resources/Private/Language/locallang_csh_tx_rkwconsultant_domain_model_contactperson.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwconsultant_domain_model_contactperson');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwconsultant_domain_model_qualification', 'EXT:rkw_consultant/Resources/Private/Language/locallang_csh_tx_rkwconsultant_domain_model_qualification.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwconsultant_domain_model_qualification');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rkwconsultant_domain_model_subservice', 'EXT:rkw_consultant/Resources/Private/Language/locallang_csh_tx_rkwconsultant_domain_model_subservice.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rkwconsultant_domain_model_subservice');
-//=================================================================
-// General stuff
-//=================================================================
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'RKW Consultant');
-
-
-//=================================================================
-// Add Flexform
-//=================================================================
-$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
-$pluginName = strtolower('Rkwconsultantmyconsultant');
-$pluginSignature = $extensionName.'_'.$pluginName;
-
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/MyConsultant.xml');
-
-
-$pluginName = strtolower('Rkwconsultant');
-$pluginSignature = $extensionName.'_'.$pluginName;
-
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/Consultant.xml');
 
